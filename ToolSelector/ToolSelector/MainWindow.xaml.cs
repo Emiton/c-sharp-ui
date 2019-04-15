87641bf455b86bd8696760f2bafc153f801e12d0
Emiton Alves <emiton_alves@my.uri.edu>
2019-04-15 10:29:40 -0400

    Added intial logic for checkboxes

--- ToolSelector/ToolSelector/MainWindow.xaml.cs
+++ ToolSelector/ToolSelector/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace ToolSelector
         {
             InitializeComponent();
         }
+
+        private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_OnClickButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckbox.IsChecked 
+                = this.AssemblyCheckbox.IsChecked 
+                = this.PlasmaCheckbox.IsChecked 
+                = this.LaserCheckbox.IsChecked 
+                = this.PurchaseCheckbox.IsChecked 
+                = this.LatheCheckbox.IsChecked 
+                = this.DrillCheckbox.IsChecked 
+                = this.FoldCheckbox.IsChecked 
+                = this.RollCheckbox.IsChecked 
+                = this.SawCheckbox.IsChecked 
+                = false;
+        }
+
+        private void Checkbox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += (string)((CheckBox) sender).Content;
+        }
     }
 }
