c7427523477bf781514aeae1e77889e9a3978d65
Emiton Alves <emiton_alves@my.uri.edu>
2019-04-15 11:19:54 -0400

    Completed initial tool selector design

--- ToolSelector/ToolSelector/MainWindow.xaml.cs
+++ ToolSelector/ToolSelector/MainWindow.xaml.cs
@@ -25,11 +25,21 @@ namespace ToolSelector
             InitializeComponent();
         }
 
+        /// <summary>
+        ///     Display popup message when a button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
         {
             MessageBox.Show($"The description is: {this.DescriptionText.Text}");
         }
 
+        /// <summary>
+        ///     Reset all form values to default
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ResetButton_OnClickButton_OnClick(object sender, RoutedEventArgs e)
         {
             this.WeldCheckbox.IsChecked 
@@ -43,11 +53,54 @@ namespace ToolSelector
                 = this.RollCheckbox.IsChecked 
                 = this.SawCheckbox.IsChecked 
                 = false;
+
+            this.NoteText.Text = "";
         }
 
+        /// <summary>
+        ///     Called when a checkbox is checked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Checkbox_OnChecked(object sender, RoutedEventArgs e)
         {
             this.LengthText.Text += (string)((CheckBox) sender).Content;
+        }
+
+        /// <summary>
+        ///     Called when dropdown selection is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FinishDropdown_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.NoteText == null)
+            {
+                return;
+            }
+            var combo = (ComboBox) sender;
+            var value = (ComboBoxItem) combo.SelectedValue;
+            this.NoteText.Text = (string) value.Content;
+        }
+
+        /// <summary>
+        ///     Set default values once the window is loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropdown_SelectionChanged(this.FinishDropdown, null);
+        }
+
+        /// <summary>
+        ///     Call when the Supplier Name text is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SupplierNameText_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = this.SupplierNameText.Text;
         }
     }
 }
